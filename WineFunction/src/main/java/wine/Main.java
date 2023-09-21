e4dc94f810ad78ba167b9571b259b9772c15f6f0
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-09-20 23:17:19 -0300

    last commit

--- WineFunction/src/main/java/wine/Main.java
+++ WineFunction/src/main/java/wine/Main.java
@@ -5,9 +5,9 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 
 public class Main {
     public static void main(String[] args) {
-        String body = "{\"name\":\"wine1\",\"price\":\"1\",\"slug\":\"wine1\",\"country\":\"country1\",\"countrySlug\":\"country1\",\"winery\":\"winery1\",\"winerySlug\":\"winery1\",\"taste\":\"taste1\",\"tasteSlug\":\"taste1\",\"age\":\"age1\",\"ratings\":\"ratings1\"}";
-        APIGatewayProxyResponseEvent res = new Handler()
-                .handleRequest(new APIGatewayProxyRequestEvent().withHttpMethod("POST").withBody(body), null);
-        System.out.println(res.toString());
+        // String body = "{\"name\":\"wine1\",\"price\":\"1\",\"slug\":\"wine1\",\"country\":\"country1\",\"countrySlug\":\"country1\",\"winery\":\"winery1\",\"winerySlug\":\"winery1\",\"taste\":\"taste1\",\"tasteSlug\":\"taste1\",\"age\":\"age1\",\"ratings\":\"ratings1\"}";
+        // APIGatewayProxyResponseEvent res = new Handler()
+                // .handleRequest(new APIGatewayProxyRequestEvent().withHttpMethod("POST").withBody(body), null);
+        // System.out.println(res.toString());
     }
 }
